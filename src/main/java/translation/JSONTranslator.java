2e085517ee2a238927d497ed2058627ef7d94278
pengfan200455 <pengfan200455@gmail.com>
2025-09-22 16:01:32 -0400

    First two parts of task C

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -52,12 +52,19 @@ public class JSONTranslator implements Translator {
                 List<String> languages = new ArrayList<>();
 
                 // TODO Task C: record this countryCode in the correct instance variable
+                if (!countryCodes.contains(countryCode)) {
+                    countryCodes.add(countryCode);
+                }
 
                 // iterate through the other keys to get the information that we need
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
                         // TODO Task C: record this translation in the appropriate instance variable
+                        // abstract country name from JSON
+                        String translation = countryData.getString(key);
+
+                        translations.put(countryCode + "-" + languageCode, translation);
 
                         if (!languages.contains(languageCode)) {
                             languages.add(languageCode);
